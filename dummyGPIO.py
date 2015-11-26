7200b5eee11d3ef5d6f25f37d80cac171545f2b1
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-27 07:21:10 +0900

    add dummyGPIO

---
+++ dummyGPIO.py
@@ -0,0 +1,35 @@
+'''
+v0.1 2015/11/26
+  - add output()
+  - add setmode()
+  - add setup()
+'''
+
+class CDummyGPIO:
+	def __init__(self):
+		self.BOARD = 0;
+		self.OUT = 1;
+		# do nothing
+		return
+
+	def setmode(self, board):
+		# do nothing
+		return
+
+	def setup(self, pinnum, inout):
+		# do nothing
+		return
+
+	def output(self, pinnum, onoff):
+		# do nothing
+		return
+
+# Usage
+
+'''
+from dummyGPIO import CDummyGPIO
+GPIO = CDummyGPIO()
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.OUT)
+'''
