206041e2161ea7f28b2eb1bda24a0da8676cb38e
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-28 07:14:58 +0900

    update version comment

--- IPadrDisplay.py
+++ IPadrDisplay.py
@@ -1,6 +1,11 @@
 from util7SegLED import info7seg_init, info7seg_on
 import time
 
+'''
+v0.1  2015/11/28
+  - display 0..9 and decimal point
+'''
+
 while True:
 	info7seg_init()
 	for idx in range(0, 11):
