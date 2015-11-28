b457193ababcc398f5ff1da1a6aa9cc696b3bafa
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-28 16:38:50 +0900

    WIP

--- IPadrDisplay.py
+++ IPadrDisplay.py
@@ -6,6 +6,7 @@ import time
 
 '''
 v0.2  2015/11/28
+  - add disp_ipAddress()
   - extract method to disp_0_9_DP()
   - use all off function
 v0.1  2015/11/28
@@ -17,11 +18,21 @@ def disp_0_9_DP(intvl):
 		for idx in range(0, 11):
 			info7seg_onOff(idx, intvl)
 
+def disp_ipAddress(ipadr, intvl_sec):
+	for idx in range(0, len(ipadr)):
+		if "." in ipadr[idx]:
+			info7seg_onDecimalPointOff(intvl_sec)
+		else:
+			val = int(ipadr[idx])
+			info7seg_onOff(val, intvl_sec)
+
 info7seg_init()
 
 for loop in range(0,3):
 	info7seg_onDecimalPointOff(0.5)
 	info7seg_allOff()
 	time.sleep(0.5)
+
+disp_ipAddress("192.168", 0.5)
 
 disp_0_9_DP(0.5)
