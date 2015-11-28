b8a350f9cdc5e28133d0778d2db02b6ba4c8d48c
yasokada <yasokada@gmail.com>
2015-11-28 16:11:45 +0900

    disp_0_9_DP() takes intvl arg

--- IPadrDisplay.py
+++ IPadrDisplay.py
@@ -10,18 +10,18 @@ v0.1  2015/11/28
   - display 0..9 and decimal point
 '''
 
-def disp_0_9_DP():
+def disp_0_9_DP(intvl):
 	while True:
 		for idx in range(0, 11):
 			info7seg_on(idx)
-			time.sleep(2)
+			time.sleep(intvl)
 
 info7seg_init()
 
 for loop in range(0,3):
 	info7seg_decimalPoint()
-	time.sleep(2)
+	time.sleep(0.5)
 	info7seg_allOff()
-	time.sleep(2)
+	time.sleep(0.5)
 
-disp_0_9_DP()
+disp_0_9_DP(0.5)
