031ba4ec8754c878c7ffe8de0969c788edf29125
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-28 15:59:24 +0900

    extract method to disp_0_9_DP()

--- IPadrDisplay.py
+++ IPadrDisplay.py
@@ -3,16 +3,19 @@ import time
 
 '''
 v0.2  2015/11/28
+  - extract method to disp_0_9_DP()
   - use all off function
 v0.1  2015/11/28
   - display 0..9 and decimal point
 '''
 
-while True:
-	info7seg_init()
-	for idx in range(0, 11):
-		info7seg_on(idx)
-		time.sleep(2)
+def disp_0_9_DP():
+	while True:
+		info7seg_init()
+		for idx in range(0, 11):
+			info7seg_on(idx)
+			time.sleep(2)
 
-	info7seg_allOff()
-	time.sleep(2)
+disp_0_9_DP()
+info7seg_allOff()
+time.sleep(2)
